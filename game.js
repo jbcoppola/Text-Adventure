ef9d1ff8b5b2c4888efa120a7b35fa339160e5a3
Joseph Coppola <jbcoppola@gmail.com>
2017-11-08 11:17:49 -0700

    added more objects and methods

--- game.js
+++ game.js
@@ -25,7 +25,6 @@ class Path {
     }
 }
 
-
 class Area {
     constructor(name, description, exits = [], items = []) {
         this.name = name;
@@ -49,5 +48,75 @@ class Area {
     removeItems(...items) {
         items.forEach(item => removeItem(item));
         return this;
+    }
+}
+
+var areaData = [{
+    "name": "Start",
+    "description": "Starting room. Boring.",
+    "exits": [{
+        "cardinal": "North",
+        "destination": "North Room",
+        "description": "Path to north room"
+    }],
+    "items": [{
+        "name": "Rock",
+        "description": "A rock.",
+        "value": 0
+    }]
+}, {
+    "name": "North Room",
+    "description": "The room in the north.",
+    "exits": [{
+        "cardinal": "South",
+        "destination": "Start Room",
+        "description": "Path to start"
+    }],
+    "items": [{
+        "name": "Stick",
+        "description": "A stick.",
+        "value": 0
+    }]
+    }];
+
+var Areas = areaData.map(area => new Area({ name, description, exits, items }));
+Areas.get = function (roomName) {
+    return Areas.find(name == roomName);
+}
+
+class Player {
+    constructor() {
+        this.inventory = [];
+        this.location = Areas.go("Start Room");
+    }
+    printInventory() {
+        for (let item of this.inventory) {
+            console.log(item);
+        }
+    }
+    transport(roomName) {
+        this.location = Areas.get(roomName);
+    }
+    move(direction) {
+        if (this.location.exits.some(cardinal === direction)) {
+            this.transport(exits.find(cardinal === direction).destination);
+        }
+    }
+    look() {
+        return this.location.description;
+    }
+    examine(object) {
+        if (this.inventory.some(name === object)) {
+            return this.inventory.find(name === object).description;
+        }
+        else if (this.location.items.some(name === object)) {
+            return this.location.items.find(name === object).description;
+        }
+        else {
+            return `I don't see that here.`;
+        }
+    }
+    take(object) {
+       
     }
 }
