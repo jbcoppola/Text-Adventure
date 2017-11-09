c7074644274a73eae2a30c018167c38612a52c9d
Joseph Coppola <jbcoppola@gmail.com>
2017-11-09 13:04:35 -0700

    added look logic

--- javascript/engine.js
+++ javascript/engine.js
@@ -1,8 +1,8 @@
 ﻿import { Item, Coin, Area, Player, Output } from 'classes';
 import { Areas } from 'area-data';
 
-
 let directions = ["north", "n", "south", "s", "east", "e", "west", "w", "southwest", "sw", "northwest", "nw", "northeast", "ne", "southwest", "se"]
+let take = ["get", "take", "steal", "grab"];
 
 function parseDirections(input) {
     switch (input) {
@@ -42,13 +42,38 @@ function parseDirections(input) {
     return input;
 }
 
-function newInput(player, input) {
+function newInput(input) {
     let output = new Output(input);
+
     input = input.toLowerCase();
-    if (directions.some(val => val === input)) {
+    input = input.split("");
+    let verb = input[0];
+
+    if (verb === "move" || verb === "go") {
+        verb = input[1];
+    }
+    if (directions.includes(verb)) {
         player.move(parseDirections(input));
         output.addWithBreaks(`You move ${input}.`);
+        return output.text;
     }
+    
+    if (verb === "look") {
+        if (input[1] === "at") {
+            verb = "examine";
+            input.splice(1, 1);
+        }
+        else if (input[1] === undefined) {
+            output.addWithBreaks(player.look());
+        }
+        else {
+            output.addWithBreaks(`I don't know what ${input[1]} is.`);
+        }
+    };
+
+    if (verb === "examine" || verb) { };
+    else if (take.includes(verb)) { };
+    else if () { };
     
     return output.text;
 }
