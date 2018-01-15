e5df9f62916fbf105a4d63f4413fd8551ab30df8
Joseph Coppola <jbcoppola@gmail.com>
2018-01-14 21:27:57 -0700

    added goop

--- modules/item-data.js
+++ modules/item-data.js
@@ -325,6 +325,10 @@ var itemData = [{
     "name": "bin",
     "aliases": ["bin", "jar bin", "rusty bin", "ground bin"],
     "description": "This bin is filled with rusted clutter, save for a single clean jar. Everything else has decayed to uselessness."
+},{
+    "name": "goop",
+    "aliases": ["glowing liquid", "liquid", "goop", "glowing goop"],
+    "description": "Goop."
 }
 ];
 
